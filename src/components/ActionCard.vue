6a9b2a8f724ad540ad3f8c6d5afa7cbb994c7463
y39 <y39scode@gmail.com>
2024-06-25 02:14:09 +0900

    Feat: date input format, 추가적인 UI 구현

--- src/components/ActionCard.vue
+++ src/components/ActionCard.vue
@@ -6,10 +6,10 @@
       label
     >
       <v-icon icon="mdi-label" start></v-icon>
-      <span>{{ action.cardTag === 'Action'? '⚡Action' : '🚀Project' }}</span>
+      <span>{{ actionCard.cardTag === 'Action'? '⚡Action' : '🚀Project' }}</span>
     </v-chip>
     <div>
-      <v-card-title v-if="!isTitleEditing" @click="editTitle">{{ action.cardTitle }}</v-card-title>
+      <v-card-title v-if="!isTitleEditing" @click="editTitle">{{ actionCard.cardTitle }}</v-card-title>
       
       <v-text-field
               v-if="isTitleEditing"
@@ -17,28 +17,35 @@
               @keyup.enter="stopTitleEdit" 
               ref="inputField"
               label="Solo"
-              v-model="action.cardTitle"
+              v-model="actionCard.cardTitle"
               variant="solo"
               class="opacity-40"
             ></v-text-field>
     </div>
     <div>
       <v-card-subtitle v-if="!isDoDateEditing" @click="editDoDate">
-        {{ action.doStartDate || 'when?' }}
+        {{ doStartDate || 'when?' }}
       </v-card-subtitle>
       <v-date-input v-if="isDoDateEditing" 
+                    class="opacity-40"
                     @blur="stopDoDateEdit"
                     clearable 
+                    show-adjacent-months
                     label="Date input" 
                     variant="solo" 
-                    v-model="action.doStartDate"
+                    placeholder="yyyy/mm/dd"
+                    prepend-icon=""
+                    prepend-inner-icon="$calendar"
+                    v-model="actionCard.doStartDate"
+                    :append-inner-icon="marker ? 'mdi-calendar-outline' : 'mdi-calendar-multiple'"
+                    @click:append-inner="toggleMarker"
                     ></v-date-input>
     </div>
-    <v-card-text>{{ action.cardContent }}</v-card-text>
+    <v-card-text>{{ actionCard.cardContent }}</v-card-text>
     <v-checkbox
             color="error"
             label="isDone"
-            v-model="action.isDone"
+            v-model="actionCard.isDone"
           ></v-checkbox>
   </v-card>
 
@@ -46,7 +53,8 @@
 
 <script setup>
 import { defineProps, nextTick, ref, watch } from 'vue';
-import { fa } from 'vuetify/locale';
+import { format } from 'date-fns';
+
 const isTitleEditing = ref(false);
 const isDoDateEditing = ref(false);
 const inputField = ref(null);
@@ -62,11 +70,20 @@ const props = defineProps(
 });
 
 
+const marker = ref(false)
+const actionCard = ref(props.action);
+const doStartDate = ref("");
+const toggleMarker = () => {
+  marker.value = !marker.value
+}
+
+
+
 const emit = defineEmits(['update:action']);
 
-watch( props.action,(newAction) => {
+watch( actionCard.value, (newAction) => {
   newAction['idx'] = props.idx; 
-  console.dir(newAction);
+  console.log("newAction11:",newAction);
   emit('update:action', newAction);
 });
 
@@ -82,10 +99,14 @@ const editDoDate = () => {
 
 const stopTitleEdit = () => {
   isTitleEditing.value = false;
+  console.log("actionCard:", actionCard.value);
 }
 
 const stopDoDateEdit = () => {
   isDoDateEditing.value = false;
+  if(actionCard.value.doStartDate){
+    doStartDate.value = format(actionCard.value.doStartDate, 'yyyy/MM/dd');
+  }
 }
 
 </script>
